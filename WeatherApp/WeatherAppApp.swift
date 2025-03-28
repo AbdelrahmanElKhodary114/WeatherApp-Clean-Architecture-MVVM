dfcebd061fe6e3db85db45bd38999a1a58db82f2
Abdulrahman Alaa <abdulrhmanalaa114@gmail.com>
2025-03-28 23:10:21 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Abdulrahman Alaa on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
